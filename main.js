d73bd617c2465fcb75a557073aeca247d67d42e8
MarcoA36 <marco_95_5@yahoo.com>
2023-07-18 17:34:17 -0300

    Se agrega el proyecto

---
+++ main.js
@@ -0,0 +1,89 @@
+
+var btn_menu = document.getElementById("btn-menu")
+var nav_desplegado = false;
+
+btn_menu.addEventListener('click', desplegarMenu)
+
+function desplegarMenu() {
+    if (nav_desplegado == false) {
+        document.getElementById("nav-menu-desplegable").style.display = "block"
+        nav_desplegado = true
+    } else {
+        document.getElementById("nav-menu-desplegable").style.display = "none"
+        nav_desplegado = false
+    }
+}
+
+const mq980 = matchMedia('(min-width:980px)');
+mq980.addEventListener('change', () => {
+    document.getElementById("nav-menu-desplegable").style.display = "none"
+    nav_desplegado = false
+})
+
+document.querySelectorAll('.btn-menu').forEach(elemento => {
+    elemento.addEventListener('click', () => {
+        if (document.getElementById("nav-menu-desplegable").style.display == "block") {
+            document.getElementById("nav-menu-desplegable").style.display = "none"
+            nav_desplegado = false
+        }
+    })
+})
+
+
+// inicio efecto maquina de escribir
+
+var titulo_inicio = document.getElementById("inicio-h1")
+var efecto_escritura = document.getElementById("efecto-escritura")
+
+
+const maquinaEscribir = (text = '', tiempo = 200, etiqueta = '') => {
+    let arrayCaracteres = text.split('')
+    etiqueta.innerHTML = ''
+    let i = 0
+    let j = text.length
+    let escribir = setInterval(function () {
+        if (i === arrayCaracteres.length) {
+            etiqueta.innerHTML = text.substring(0, j)
+            j--
+            if (j === 0) {
+                etiqueta.innerHTML = ''
+                i = 0
+                j = text.length
+            }
+        } else {
+            etiqueta.innerHTML += arrayCaracteres[i]
+            i++
+        }
+    }, tiempo)
+}
+
+maquinaEscribir(efecto_escritura.innerHTML, 140, efecto_escritura)
+
+
+// ABRIR ARTICULO
+
+document.querySelectorAll('.articulo').forEach(articulo => {
+
+    articulo.addEventListener('click', () => {
+        window.scroll({
+            top: 2100,
+            behavior: 'smooth'
+          });
+       
+        let html_estructura = articulo.innerHTML
+        document.getElementById("pop").style.display="flex";
+        document.getElementById("cont-pop").innerHTML=html_estructura;
+        // document.getElementById("cont-articulo").innerHTML=html_estructura;
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
